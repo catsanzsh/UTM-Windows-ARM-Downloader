f627e6986ba46fabc9c343f4f70aefde0a0601d0
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2021-10-27 12:56:32 -0700

    UTM Windows 10 Spice Desktop Manager 0.1
    Downloader patch update Oct 27 2021

---
+++ UTM-Windows11-Downloader.sh
@@ -0,0 +1,13 @@
+## download spice desktop tools iso from website
+printf "Downloading spice desktop tools iso from website\n"
+wget -O spice-desktop-tools.iso https://www.spice-space.org/download/spice-desktop-tools/spice-desktop-tools-latest.iso
+printf "Downloading spice desktop tools iso from website done\n"
+## Downloading Windows 10 ARM UTM disk from utm website
+printf "Downloading Windows 10 ARM UTM disk from utm's offical gallery"
+wget -O utm.iso https://www.utm.com/wp-content/uploads/2019/03/UTM-Windows10-ARM-x64-en-US-Installer.iso
+print("Download complete")
+ ## Exiting script
+printf "Exiting script\n"
+exit 0
+## End of script
+## (C ) - Flames 2021
