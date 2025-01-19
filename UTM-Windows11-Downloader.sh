f5500af8b6868a4bd4a3160c2b8d8a24d56e97b5
catsanzsh <84363801+catsanzsh@users.noreply.github.com>
2025-01-19 12:07:54 -0800

    Update UTM-Windows11-Downloader.sh

--- UTM-Windows11-Downloader.sh
+++ UTM-Windows11-Downloader.sh
@@ -1,13 +1,18 @@
-## download spice desktop tools iso from website
-printf "Downloading spice desktop tools iso from website\n"
+#!/bin/bash
+
+# Downloading Spice Desktop Tools ISO
+printf "Downloading Spice Desktop Tools ISO from website...\n"
 wget -O spice-desktop-tools.iso https://www.spice-space.org/download/spice-desktop-tools/spice-desktop-tools-latest.iso
-printf "Downloading spice desktop tools iso from website done\n"
-## Downloading Windows 10 ARM UTM disk from utm website
-printf "Downloading Windows 10 ARM UTM disk from utm's offical gallery"
+printf "Downloading Spice Desktop Tools ISO: Done\n"
+
+# Downloading Windows 10 ARM UTM disk from the official gallery
+printf "Downloading Windows 10 ARM UTM disk from UTM's official gallery...\n"
 wget -O utm.iso https://www.utm.com/wp-content/uploads/2019/03/UTM-Windows10-ARM-x64-en-US-Installer.iso
-print("Download complete")
- ## Exiting script
+printf "Downloading Windows 10 ARM UTM disk: Complete\n"
+
+# Exiting script
 printf "Exiting script\n"
 exit 0
-## End of script
-## (C ) - Flames 2021
+
+# End of script
+# (C) - Flames 2021: And then the cat ported Windows 11 to M1 Mac!
